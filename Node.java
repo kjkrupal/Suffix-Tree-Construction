94d036afef24c1a8392992062140e98b1b6a7326
AgAnkitGawde <gawde17@gmail.com>
2018-03-13 12:24:19 -0700

    l

--- Node.java
+++ Node.java
@@ -1,5 +1,17 @@
 class Node{
-  int data;
+  char data;
   Node parent;
-  Node child;
+  Node child[] = new char[sigma+1];
+}
+public class Node insert_node(char x){
+if(x == null || input.size() == 0){
+              throw new IllegalArgumentException("Input is null or doesn't contain any elements");
+          }
+
+}
+
+void setparent(Node n){
+  this.setparent=n;
+  if(n!=null)
+  n.parent=this;
 }
