3c3c013f3326bb2bef39f67f90d432bef89220e0
kjkrupal <kjkrupal@gmail.com>
2018-03-16 12:47:48 -0700

    Tree creation #1

--- SuffixTree.java
+++ SuffixTree.java
@@ -2,92 +2,60 @@ public class SuffixTree{
   String sequence_name;
   String sequence;
 
-  char[] sequence_array;
+  char[] string;
   char[] alphabet;
 
-  int sequence_length;
+  int n;
   int alphabet_length;
 
   public SuffixTree(String sequence, String sequence_name, char[] alphabet){
     this.sequence_name = sequence_name;
     this.sequence = sequence + "$";
-    this.sequence_length = sequence.length();
-    this.sequence_array = this.sequence.toCharArray();
+    this.n = sequence.length();
+    this.string = this.sequence.toCharArray();
     this.alphabet = alphabet;
     this.alphabet_length = alphabet.length;
   }
 
   public void beginTreeConstructionProcess(){
 
-    int i = 0; //checking sequence
-    int j = 0; //for checking internal node
-    int k = 1; //for keeping count of nodes
-    Node node[] = new Node[2 * sequence_length - 1];
-    Node node[0] = new Node(0, null, -1, -1, null, 0);
+    int i = 0;
 
-    while(sequence_array[i] != '$'){
-      j = 0;
+    //Create root node with id 0 and everything set to null
+    Node root = new Node(0, null, -1, -1, null, 0);
 
-      if(node[j].children.get(sequence_array[i]) != null){
-        findPath(node[j].children.get(sequence_array[i]), i, k, j);
-      }
-      else{
-        node[k] = new Node(k, node[j], i, sequence_length, null, sequence_length - i);
-        k++;
-        node[j].children.put(sequence_array[i], node[k]);
-      }
+    //Begin iteration for string starting from 0 till n
+    while(string[i] != '$'){
+
+      //Call findPath method with root as present node because this while will always begin from root node
+      findPath(root, i);
+
+      //findPath creates node for i-th suffix, so increment i for next iteration
       i++;
-
     }
-      /*if(check hashmap of node[j] and sequence_array[i]){
-        call a function( for matching the children's edge_label of n[j] and sequence_array[i])
-        else
-        make a new node with with i as start n[j] as parent
-      }*/
-
-    public void findPath(Node present, int i, int k, int j){
-
-      int n = present.end - present.start;
-      int p = present.start;
-
-      while(n != 0){
-        // Match case
-        if(sequence_array[i] == sequence_array[p]){
-          n--;
-          p++;
-          i++;
-        }
-        else{
-          //Mismatch case
-          if(present.children != null){
-            node[k] = new Node(k, node[j], p, present.end, node[j].children, sequence_length - i);
-            node[j].children.clear();
-            node[j].children.put(sequence_array[p], node[k]);
-            k++;
-            node[k] = new Node(k, node[j], i, sequence_length, null, sequence_length - i);
-            node[j].children.put(sequnce_array[i], node[k]);
-            k++
-            */
-          }
-          else{
-          /*node[k] = new Node(k, node[j], p, present.end, null aayega samjhne ki baat hai, sequence_length - i);
-            node[j].children me node k ka hash dal
-            k++;
-            node[k++] = new Node(k, node[j], i, sequence_length, null, sequence_length - i);
-            node[j].children me node k ka hash dal
-            */
-            node[k] = new Node(k, node[j], i, sequence_length, null, sequence_length - i);
-            k++;
-            node[j].children.put(sequence_array[i], node[k]);
-          }
-
-          }
-        }
-        if(n == 0){
-          check(if any hashmap (send parameters j and i=(i+length.edge_label)))
-        }
-      }
 
   }
 
+  public void findPath(Node present, int i){
+
+    //The following block executes when the present node doesn't have any children
+    if(present.children == null){
+
+      //Since no children create a new child
+      present.children = new HashMap<char, Node>();
+
+      //Put key-value pair in the newly created HashMap
+      present.children.put(string[i], new Node(i + 1, present, i, n, null, n - i));
+    }
+
+    /* The following block executes when the present node has a child but
+    doesn't have a child corresponding to current character in the string */
+
+    else if(present.children.get(string[i]) == null){
+
+      //Add a new child in the existing HashMap
+      present.children.put(string[i], new Node(i + 1, present, i, n, null, n - i))
+    }
+
+  }
 }
--- Test.java
+++ Test.java
@@ -1,25 +1,13 @@
+import  java.util.HashMap;
+
 class Test{
-  int id;
-  int num;
-  int max;
+
   public static void main(String[] args) {
 
-    Runtime r = Runtime.getRuntime();
+    HashMap map = new HashMap();
+    map.put("1", "Krupal");
 
-    System.out.println(r.maxMemory() / 1024);
-
-
-    int a = 30000000;
-    Test t[] = new Test[a];
-
-    for(int i = 0; i < a; i++){
-      t[i] = new Test();
-
-
-    }
-    Runtime s = Runtime.getRuntime();
-
-    System.out.println(s.freeMemory() / 1024);
+    System.out.println(map.get("2"));
 
   }
 }
