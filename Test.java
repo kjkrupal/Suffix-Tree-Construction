06ea1db1003e97997592d9ef64a320428f5a7707
kjkrupal <kjkrupal@gmail.com>
2018-03-25 02:06:01 -0700

    Finally done!!

--- Test.java
+++ Test.java
@@ -1,20 +1,11 @@
-import  java.util.TreeMap;
-import java.util.Map;
-
 class Test{
 
   public static void main(String[] args) {
 
-    Map<String, String> map = new TreeMap<String, String>();
-    map.put("1", "Krupal");
-    map.put("3", "Ankit");
-    map.put("4", "Bhavesh");
-    map.put("2", "Deepesh");
-    map.put("2", "Tanvi");
+    String s = "Krupal Jadhav";
 
-    for (String id : map.keySet()) {
-      System.out.println(id);
-    }
-    System.out.println(map.get("2"));
+    String sub = s.substring(1, 10);
+    
+    System.out.println(sub.substring(1, 4));
   }
 }
